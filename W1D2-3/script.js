620795daa995df53c5fa3e851d3d48bd7b18c355
Irenevs83 <80913299+Irenevs83@users.noreply.github.com>
2021-05-05 18:30:59 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,13 @@
+console.log('Hallo Winc Academy studenten')
+// Dit is een grote som.
+// Dit zie je niet in node omdat het een comment is.
+
+/*
+let multiplication = 1230941 * 1823794;
+console.log("uitkomst", multiplication);
+
+let division = 637263 / 54;
+console.log("uitkomst", division);
+*/
+
+// De uitkomst van de sommen zie je niet meer omdat deze in de blok comments staat.
